640d114936d6f2d878674876014119bfef4d7865
Thabang <tp.softwaredev@gmail.com>
2024-06-21 10:38:12 -0500

    Changed the timetable format and class schedule URL

--- utils/URLs.py
+++ utils/URLs.py
@@ -2,5 +2,5 @@ from enum import Enum
 
 class URLs(Enum):
     GET_CLASS_SCHEDULE = ""
-    GET_CLASS_SCHEDULE_TEST = "https://081c4a2a-6343-49a2-9c38-1835834d8a00.mock.pstmn.io/Classes" #Newest URL
+    GET_CLASS_SCHEDULE_TEST = "https://e11edae8-1211-437c-9346-26a4949c8d4a.mock.pstmn.io" #Newest URL
     AUTHENTICATE = "https://test.tmwork.net/2023.1/api/ops/auth"
